590c8a7522500de7bfc90ad9516c6e90ec9b5f95
Yurii Pakhota <ov3rfordream@gmail.com>
2025-04-21 13:37:43 +0300

    Create the first implementation of the BooksModule

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Book, BookCreationAttrs } from './books.entity';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  private readonly books: Book[] = [];
+
+  findAll(): Book[] {
+    return this.books;
+  }
+
+  create(book: BookCreationAttrs): Book {
+    const newBook = {
+      ...book,
+      id: `${this.books.length + 1}`,
+    } as Book;
+
+    this.books.push(newBook);
+
+    return newBook;
+  }
+}
