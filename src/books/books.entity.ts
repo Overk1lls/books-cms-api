22332175431df424211ec33d3e6a157eec023eca
Yurii Pakhota <ov3rfordream@gmail.com>
2025-04-21 13:58:37 +0300

    Create a PG table & entity for the books

--- src/books/books.entity.ts
+++ src/books/books.entity.ts
@@ -1,22 +1,30 @@
-import { ObjectType, Field, ID } from '@nestjs/graphql';
-import { CreationAttributes, EntityOptional } from '../app.types';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { EntityAttributes, EntityOptional } from '../app.types';
 
+@Entity({ name: 'books' })
 @ObjectType()
 export class Book {
+  @PrimaryGeneratedColumn('uuid')
   @Field(() => ID)
   id: string;
 
+  @Column()
   @Field()
   title: string;
 
+  @Column()
   @Field()
   author: string;
 
+  @Column()
   @Field()
   publicationDate: string;
 
+  @Column({ nullable: true })
   @Field({ nullable: true })
   genre: string;
 }
 
-export type BookCreationAttrs = EntityOptional<CreationAttributes<Book>, 'genre'>;
+export type BookCreationAttrs = EntityOptional<EntityAttributes<Book>, 'genre'>;
+export type BookUpdateAttrs = EntityAttributes<Book>;
